737b0da7ef16c65427381fe37696ce02491b3f4f
Kostikzzz <k.lapshov@gmail.com>
2016-08-14 13:28:09 +0300

    >>>

--- seasia/itinerary.py
+++ seasia/itinerary.py
@@ -17,7 +17,7 @@ class Itinerary():
             if p['cur'] == 0:
                 add = p['min']
 
-            if p['cur']+add < p[limit] and add <= self.fn and p[factor] > best_pop:
+            if p['cur']+add < p[limit] and add <= self.fn and p[factor] > best_pop and ix != len(self.points):
                 best_pop = p[factor]
                 best_index = ix
                 best_add = add
@@ -25,6 +25,7 @@ class Itinerary():
         if best_index >= 0:
             self.points[best_index]['cur'] += best_add
             self.fn -= best_add
+            print ('fn a2b: %d' % self.fn)
             return True
         else:
             return False
@@ -32,7 +33,9 @@ class Itinerary():
     def __init__(self, sr, q):
         self.query = q
         self.points = json.loads(sr.routeData)
-        self.fn = q["duration"]-3  # free nights
+        self.fn = q["duration"]-5  # free nights
+        self.roundtrip = True if self.points[0] == self.points[-1] else False
+        print ('fn init: %d' % self.fn)
         for p in self.points:
 
             if "must" in p:
@@ -54,15 +57,17 @@ class Itinerary():
                 if val:
                     p['pop'] += p[key]
 
-        self.points[0]['cur'] = 1
-        self.points[-1]['cur'] = 1
+        self.points[0]['cur'] = 2
+        self.points[-1]['cur'] = 2
 
+        selected_cats = []
         for c in self.categories:
             if c in q['activitiesGroup'] and q['activitiesGroup'][c] is True:
                 self.addToBest(c, 'rmx')
+                if c not in selected_cats:
+                    selected_cats.append(c)
 
         if 'pace' in q and q['pace']==1:  # if pace == fast
-            print ('1st pass')
             res = True
             while self.fn > 0 and res is True:
                 res = self.addToBest('pop', 'rmn')
@@ -71,9 +76,30 @@ class Itinerary():
         while self.fn > 0 and res is True:
             res = self.addToBest('pop', 'rmx')
 
+        # evaluate result
+        relevance = 0
+        if len(selected_cats) > 0:
+            for c in selected_cats:
+                for p in self.points:
+                    relevance += p[c]*p['cur']
+            relevance = relevance / q['duration'] / len(selected_cats)
+
+        else:
+            for p in self.points:
+                relevance+=p['pop']*p['cur']
+            relevance = relevance / q['duration']
+
+        self.relevance = relevance
+
+        real_length = 0
+        for p in self.points:
+            if p['cur']>0:
+                real_length+=1
+        self.real_pace = q['duration']/real_length
+
 
     def get_report(self):
-        text = '>>> '
+        text = 'R: '+str(self.relevance)+' P: '+str(self.real_pace)+' | '
         for p in self.points:
             if p['cur']>0:
                 text += "%s: %d, " % (p['text'], p['cur'])
